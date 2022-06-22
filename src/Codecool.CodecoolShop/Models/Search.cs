023b845813970ec7d9419da332b3633e8c6af90e
GrzegorzWikar <grzegorzwikar938@gmail.com>
2022-06-22 14:29:30 +0200

    Search

---
+++ src/Codecool.CodecoolShop/Models/Search.cs
@@ -0,0 +1,9 @@
+﻿namespace Codecool.CodecoolShop.Models
+{
+    public class Search
+    {
+        public string? Supplier { get; set; }
+
+        public string? ProductCategory { get; set; }
+    }
+}
---
+++ src/Codecool.CodecoolShop/ViewModels/ProdactListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Codecool.CodecoolShop.Models;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.ViewModels
+{
+    public class ProdactListViewModel
+    {
+        public List<Product> Products { get; set; }
+
+        public List<Supplier> Suppliers { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
