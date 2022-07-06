914bc43eaa0826af7322f2479bad5a5906992874
RogerWitkowski <witkowski.roger@gmail.com>
2022-07-06 14:11:16 +0200

    roles created

--- src/Codecool.CodecoolShop/Program.cs
+++ src/Codecool.CodecoolShop/Program.cs
@@ -1,8 +1,10 @@
 using Codecool.DataAccess;
 using Codecool.DataAccess.Repository;
 using Codecool.DataAccess.Repository.IRepository;
+using Codecool.Utility;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,9 +24,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()  //options => options.SignIn.RequireConfirmedAccount = true
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddDefaultTokenProviders()  //options => options.SignIn.RequireConfirmedAccount = true
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
